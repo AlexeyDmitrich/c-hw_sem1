1000a3db5f6983b0a691615991fb40fb4b3eddd6
Alexey <a.d.kamenev@yandex.ru>
2022-11-23 11:30:13 +0300

    4th quest complited

---
+++ sem1hw/quest4/Program.cs
@@ -0,0 +1,46 @@
+// принимаем число, возвращаем все четные числа от 1 до него
+int module (int num){
+    if (num >= 0) {
+        return num;
+    }
+    else {
+        return (num * (-1));
+    }
+}
+
+Boolean isEven (int num){
+if ((module(num)%2) == 0){
+    return true;
+}else{
+return false;
+}
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number == 0){
+    Console.WriteLine("Это ноль");
+}else {
+    if (number > 0){
+        for(int i=2; i<number; i++){
+            if (isEven(i)){
+            Console.Write(i);
+            Console.Write(", ");
+            }
+        }
+        if (isEven(number)){
+        Console.WriteLine(number);
+        }
+    } else {
+        for(int i=-2; i>number; i--){
+            if (isEven(i)){
+            Console.Write(i);
+            Console.Write(", ");
+            }
+        }
+        if (isEven(number)){
+        Console.Write(number);
+        }
+    }
+}
+Console.WriteLine();
