c8e4d6b1cd439c2538233fc4b3164e48601c7e4a
Alexey <a.d.kamenev@yandex.ru>
2022-11-23 09:13:06 +0300

    1st quest complited

---
+++ sem1hw/quest1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2){
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}else if (num2 > num1){
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}else{
+    Console.WriteLine("Числа равны");
+}
