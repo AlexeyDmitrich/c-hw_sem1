ec9b13deb118c188f58c1ead8c9f2d4abfcee100
Alexey <a.d.kamenev@yandex.ru>
2022-11-23 10:59:31 +0300

    3rd quest complited

--- sem1hw/quest3/Program.cs
+++ sem1hw/quest3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int module (int num){
+    if (num >= 0) {
+        return num;
+    }
+    else {
+        return (num * (-1));
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number == 0){
+    Console.WriteLine("Это ноль");
+}else if ((module(number)%2) == 0){
+    Console.WriteLine("Это число четное");
+}else{
+Console.WriteLine("Это число нечетное");
+}
